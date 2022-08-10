e76c64b4bf66b8302bd66c49889e7d249b1350e7
Monim Hossain <flynmonim@gmail.com>
2022-08-10 17:38:14 +0600

    two sum update

--- twoSum.js
+++ twoSum.js
@@ -4,14 +4,12 @@
  * @return {number[]}
  */
  var twoSum = function(nums, target) {
-    let result = [];
-    for(let i=0; i<nums.length; i++){
-        for(let j=i+1; j<nums.length; j++){
-            if((nums[i] + nums[j]) === target){
-                result.push(i, j);
-                break;
-            }   
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (target - nums[i] in map) {
+            return [map[target - nums[i]], i];
+        } else {
+            map[nums[i]] = i;
         }
     }
-    return result;
 };
